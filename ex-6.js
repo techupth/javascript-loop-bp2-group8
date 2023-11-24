ecadb304ca0781ee3e8e1dd101b1d097b49ba9c9
fonytas <kunagit.jb@gmail.com>
2023-11-24 23:06:56 +0700

    Feat Assignments Loop : finished assignments

--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,21 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+let split=companyName.split("")
 
+
+for(let i=split.length-1;i>=0;i--){
+ reversedCompanyName+=split[i]
+}
 console.log(reversedCompanyName);
+
+
+// function reverseWord (str){
+//     let split = str.split('');
+//     let reverseResult='';
+// for(let i=split.length-1 ; i>=0;i--){
+//     reverseResult+= split[i]   
+// }
+// return reverseResult
+// }
+// console.log(reverseWord('test'))
